ff946c34cc23e4b8fea1c86da47de8c88f91b197
Javier Cubero <102825817+JaviCubero@users.noreply.github.com>
2025-05-23 11:25:36 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let full_excuse = [];
+
+  let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '/*, 'The neighbour '*/];
+  let action = ['ate ', 'peed ', 'crushed ', 'broke '/*, 'sold '*/];
+  let what = ['my homework ', 'my phone ', 'the car '/*, 'the door ', 'my trousers '*/];
+  let when = ['before the class.', 'when I was sleeping.', 'while I was exercising.', 'during my lunch.', 'while I was praying.'];
+
+  function randomNumber (array_lenght) {
+    let randomNumber = 7;
+
+		for (randomNumber; randomNumber > (array_lenght-1); randomNumber++) {
+			randomNumber = Math.floor(Math.random()*10-1);
+		}
+    
+    return randomNumber;
+  }
+    
+  full_excuse += who[randomNumber(who.length)];
+  full_excuse += action[randomNumber(action.length)];
+  full_excuse += what[randomNumber(what.length)];
+  full_excuse += when[randomNumber(when.length)];
+
+  const excuse = document.querySelector(".excuse")
+
+  excuse.innerHTML = full_excuse;
 };
